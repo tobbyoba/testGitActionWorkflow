5ca4ad957e6d4f6751fe196c40b11bb5e7f890a5
olutoba obawemimo <tobbyoba86@gmail.com>
2023-09-19 17:22:52 -0500

    9th commit

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -1,8 +1,8 @@
 resource "aws_vpc" "test" {
-    cidr_block = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
+  instance_tenancy = "default"
+  tags = {
+    Name = "oguns"
+  }
 
-    tags = {
-      Name = "oguns"
-    }
-  
 }
